95d7d8b7dc58fde7cbc1d0facf9ab3c6f8b73461
ibrahim salah <ibrahim.salah.officiel@gmail.com>
2025-11-01 19:23:59 +0100

    solution

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: './.env'});
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const path = require('path')
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout', '../views/layouts/layout')
+
+app.use((req, res)=>{
+    res.render('pages/home', {nickname: "taha", sex: "male"});
+})
+
+
+app.listen(port, ()=>{
+    console.log(`le serveur ecoute sur http:127.0.0.1:${port}`);
+})
